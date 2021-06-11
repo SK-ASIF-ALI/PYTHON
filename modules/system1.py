3d80a8f649c93ff361a2b5cb1e519ec5d8110c98
SK ASIF ALI <58936617+SK-ASIF-ALI@users.noreply.github.com>
2021-06-11 18:31:28 +0530

    Add files via upload

---
+++ modules/system1.py
@@ -0,0 +1,4 @@
+## This will act as library file and will be imported in user.py file
+def welcome(name):
+    print("Welcome To Rbwecom ",name)
+
---
+++ modules/user.py
@@ -0,0 +1,4 @@
+import system1 as sys
+## We are trying to import the system1 file as sys
+print(sys.welcome("Roy"))
+## Then we have called the function of welcome which reside under the system1
